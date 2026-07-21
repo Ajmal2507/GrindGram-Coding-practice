da310ef36db525e64bd839cde907253537e0ceef
Ajmal2507 <ajmal25k@gmail.com>
2026-07-21 22:34:43 +0530

    Added comments in divisorofanum.java file

--- Divisorofanum.java
+++ Divisorofanum.java
@@ -1,3 +1,36 @@
+/*
+ Print all Divisors of a number
+Easy
+40/40
+Average time to solve is 15m
+Contributed by
+500 upvotes
+Asked in companies
+Problem statement
+Given an integer ‘N’, your task is to write a program that returns all the divisors of ‘N’ in ascending order.
+
+
+
+For example:
+'N' = 5.
+The divisors of 5 are 1, 5.
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1 :
+10
+Sample Output 1 :
+1 2 5 10
+Explanation of Sample Input 1:
+The divisors of 10 are 1,2,5,10.
+Sample Input 2 :
+6
+Sample Output 2 :
+1 2 3 6
+Explanation of Sample Input 2:
+The divisors of 6 are 1, 2, 3, and 6.
+Constraints :
+1 <= 'N' <= 10^5
+*/
+
 import java.util.*;
 
 public class Divisorofanum {
