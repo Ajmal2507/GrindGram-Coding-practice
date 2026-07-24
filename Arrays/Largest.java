33f9c0217a8e5bfc8cff9f5bca131a71a0dda5d5
Ajmal2507 <ajmal25k@gmail.com>
2026-07-24 20:51:55 +0530

    Second Largest

--- Arrays/Largest.java
+++ Arrays/Largest.java
@@ -1,6 +1,8 @@
 package Arrays;
 
 public class Largest {
+
+    // First Largest element...
     static int large = 0;
     static int Solution(int[] arr){
         for(int i=0;i<arr.length;i++){
@@ -10,8 +12,26 @@ public class Largest {
         }
         return large;
     }
+
+    // Second Largest element...
+    static int flarge = 0;
+    static int slarge = 0;
+    static int Ssolution(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>flarge){
+                slarge = flarge;
+                flarge = arr[i];
+            }
+            if(arr[i]>slarge && arr[i]!=flarge){
+                slarge = arr[i];
+            }
+        }
+        return slarge;
+    }
+
     public static void main(String[] args) {
-         int[] arr = {1,2,3,4,2,6,9,4};
+         int[] arr = {11,2,3,4,2,66,9,85,4};
          System.out.println("Largest Element in the array is: "+Solution(arr));
+         System.out.println("The Second largest element is: "+Ssolution(arr));
     }
 }
