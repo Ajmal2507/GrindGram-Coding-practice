a285e57bd9303b850b114725768da6d8cc3e2ca0
Ajmal2507 <ajmal25k@gmail.com>
2026-08-09 22:54:31 +0530

    Three sum problem

---
+++ Arrays/Two_Pointer/ThreeSum.java
@@ -0,0 +1,45 @@
+package Two_Pointer;
+
+import java.util.HashSet;
+/*
+Triplet Sum in Array
+Solved
+Difficulty: MediumAccuracy: 35.0%Submissions: 387K+Points: 4Average Time: 15m
+Given an array arr[] and an integer target, determine if there exists a triplet in the array whose sum equals the given target.
+
+Return true if such a triplet exists, otherwise, return false.
+
+Examples: 
+
+Input: arr[] = [1, 4, 45, 6, 10, 8], target = 13
+Output: true 
+Explanation: The triplet {1, 4, 8} sums up to 13.
+Input: arr[] = [1, 2, 4, 3, 6, 7], target = 10
+Output: true 
+Explanation: The triplets {1, 3, 6} and {1, 2, 7} both sum to 10. 
+Input: arr[] = [40, 20, 10, 3, 6, 7], target = 24
+Output: false 
+Explanation: No triplet in the array sums to 24.
+*/
+public class ThreeSum {
+    public static boolean solution(int[] arr, int target){
+        int n = arr.length;
+
+        for(int i=0;i<n-2;i++){
+            HashSet<Integer> set = new HashSet<>();
+            for(int j=i+1;j<n;j++){
+                int remainder = target - arr[i] - arr[j];
+                if(set.contains(remainder)){
+                    return true;
+                }
+                set.add(arr[j]);
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 45, 6, 10, 8};
+        int target = 13;
+        System.out.println(solution(arr,target));
+    }
+}
