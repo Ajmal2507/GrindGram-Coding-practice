a58eb4df53dbf767c3b0d75fe0cc5e64d47e8f23
Ajmal2507 <ajmal25k@gmail.com>
2026-08-10 20:43:41 +0530

    Added problem statement

--- Arrays/Two_Pointer/FindIntersection.java
+++ Arrays/Two_Pointer/FindIntersection.java
@@ -1,4 +1,24 @@
 import java.util.*;
+/*
+Intersection of Arrays with Distinct
+Solved
+Difficulty: EasyAccuracy: 32.83%Submissions: 243K+Points: 2Average Time: 20m
+Given two unsorted integer arrays a[] and b[] each consisting of distinct elements, the task is to return the count of elements in the intersection (or common elements) of the two arrays.
+
+Intersection of two arrays can be defined as the set containing distinct common elements between the two arrays. 
+
+Examples:
+
+Input: a[] = [89, 24, 75, 11, 23], b[] = [89, 2, 4]
+Output: 1
+Explanation: 89 is the only element in the intersection of two arrays.
+Input: a[] = [1, 2, 4, 3, 5, 6], b[] = [3, 4, 5, 6, 7]
+Output: 4
+Explanation: 3, 4, 5, and 6 are the elements in the intersection of two arrays.
+Input: a[] = [20, 10, 30, 50, 40], b[] = [15, 25, 30, 20, 35]
+Output: 2
+Explanation: 20 and 30 are the elements in the intersection of the two arrays.
+*/
 public class FindIntersection {
     public static int solution(int[] a, int[] b){
         Set<Integer> set = new HashSet<>();
