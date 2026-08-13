6789d6658b4724741b902eb824189cbc69c4b176
Ajmal2507 <ajmal25k@gmail.com>
2026-08-13 20:17:01 +0530

    added question

--- Mock_Test_1/Count_Digits.java
+++ Mock_Test_1/Count_Digits.java
@@ -1,5 +1,16 @@
 package Mock_Test_1;
+/*
+Section A — Basic Math
 
+Q1. Count Digits
+
+Given an integer n, return the number of digits.
+
+Example:
+
+Input: 12345
+Output: 5
+*/
 public class Count_Digits {
     public static int solution(int num){
         int count = 0;
