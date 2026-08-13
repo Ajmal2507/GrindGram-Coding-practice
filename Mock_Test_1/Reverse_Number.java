7fd0a251c55d37594da6fa66d9221e6b1c6c64fa
Ajmal2507 <ajmal25k@gmail.com>
2026-08-13 20:21:39 +0530

    question

--- Mock_Test_1/Reverse_Number.java
+++ Mock_Test_1/Reverse_Number.java
@@ -1,6 +1,18 @@
 package Mock_Test_1;
 
 public class Reverse_Number {
+    /*
+    Q2. Reverse Number
+
+Reverse the digits of an integer.
+
+Example:
+
+Input: 12340
+Output: 4321
+
+Try to handle 0 correctly.
+    */
     public static void main(String[] args) {
         int num = 12340;
         int rev = 0;
