8e4f806cde6b4a21f9e5c17850e9228d726d85ba
Ajmal2507 <ajmal25k@gmail.com>
2026-08-22 18:19:06 +0530

    Count Inversions Brute Force Approach

--- Arrays/AdvancedProblems/CountInversions.java
+++ Arrays/AdvancedProblems/CountInversions.java
@@ -1,9 +1,18 @@
 public class CountInversions{
     public static int inversionCount(int[] arr){
-        return 0;
+        int count = 0;
+        for(int i = 0;i<arr.length;i++){
+            for(int j = arr.length-1;j>=0;j--){
+                if(i<j && arr[i]>arr[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
     public static void main(String[] args) {
-        int arr[] = {4, 3, 2, 1};
-        System.out.println(inversionCount(arr));
+        // int arr[] = {2, 4, 1, 3, 5};
+        int arr2[] = {10, 10, 10};
+        System.out.println(inversionCount(arr2));
     }
 }
