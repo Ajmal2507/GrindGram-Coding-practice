649a13c045f7c9aa693d0413e969b3348ff00eaf
Ajmal2507 <ajmal25k@gmail.com>
2026-08-22 18:14:46 +0530

    start panuvomaa

---
+++ Arrays/AdvancedProblems/CountInversions.java
@@ -0,0 +1,9 @@
+public class CountInversions{
+    public static int inversionCount(int[] arr){
+        return 0;
+    }
+    public static void main(String[] args) {
+        int arr[] = {4, 3, 2, 1};
+        System.out.println(inversionCount(arr));
+    }
+}
